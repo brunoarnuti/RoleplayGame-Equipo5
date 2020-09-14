e1e2e8e97a357cb1beccf816af1010836da6892c
brunoarnuti <brunoarnuti@gmail.com>
2020-09-14 20:33:04 -0300

    arreglos

--- src/Library/ComposedItem.cs
+++ src/Library/ComposedItem.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 
 namespace RolGame
 {
+    /*
+    Esta clase se creo con el objetivo de representar items que pueden agregar a otros items, de esta forma modificando su poder.
+    La clase solo colabora con su clase padre (Item) ya que le delega muchas de sus funciones.
+    A su vez, por ahora, es la clase padre de SpellBook, ya que los objetos SpellBook son objetos compuestos.
+    Esta clase cumple con el SRP, ya que solo cambiara si se quiere cambiar la forma en la que se representan los items compuestos.
+
+    No tenia sentido agregar una lista de componentes a la clase (Item), ya que en seria obsoleta en caso de que se trate de un item simple.
+    Esta clase sobre-escribe un par de metodos, es un ejemplo del patron GRASP de Polimorfismo.
+    La manera en la que se lleva a cabo el metodo (GetItemAttack y GetItemDefense) es diferente dependiendo de si es un item compuesto o no
+    por ese mismo motivo es conveniente usar una operacion polimorfica.
+    
+
+    Tambien es importante descatar que el modelo que involucra a la clase Item, ComposedItem, SpellBook y Spell fue influenciado por el patron Composite.
+    La imagen de el modelado de este mismo patron esta en una imagen en la carpeta Program.
+    */
     public class ComposedItem : Item
     {
-        private List<Item> components;
+        //Lista de items componentes
+        protected List<Item> components;
+        //Variable auxiliar
         private int baseAttack;
+        //Variable auxiliar
         private int baseDefense;
 
         public List<Item> Components
@@ -45,7 +63,7 @@ namespace RolGame
             return result;
         }
 
-        public void AddComponent(Item component)
+        public virtual void AddComponent(Item component)
         {
             this.components.Add(component);
             this.UpdateComposedItemStats();
--- src/Library/SpellBook.cs
+++ src/Library/SpellBook.cs
@@ -5,8 +5,18 @@ namespace RolGame
 {
     public class SpellBook : ComposedItem
     {
+
         public SpellBook(string name, int attack, int defense) : base(name, attack, defense)
         {
+        }
+
+        public override void AddComponent(Item spell)
+        {
+            //Verifica si el item a agregar es un hechizo, caso contrario no lo agregara.
+            if(spell.GetType() == typeof(Spell))
+            {
+                this.components.Add(spell);
+            }
         }
     }
 }
